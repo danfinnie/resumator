446cd7160c5d67f66ee1f56cf9c5220b5ae01d24
Dan Finnie <dan@danfinnie.com>
2014-07-05 20:54:34 -0700

    Add sample data to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Job.delete_all
+
+Job.create!(started: '05 Nov 2012', ended: '05 Jul 2013', title: "Full Stack Web Developer", place: "Noise New York", description: <<-EOT)
+  * Implement browser-based video chatting and appointment scheduling,
+  interface with third-party POS system for promotion that increased same-store
+  sales by 25% over stores without promotion.
+  * Grow video library microsite to maintainable platform designed to easily
+  support limited-time campaigns.
+EOT
+
+Job.create!(started: '05 Aug 2012', ended: '05 Nov 2013', title: "Backend Developer", place: "Plum Analytics (contract)", description: <<-EOT)
+  * Harvest and crawl 20+ web APIs (REST, data dumps, and screen scraping) in a
+  reliable and scalable manner. Merge similar records across data streams and
+  transform data to fit internal schemas and classifications.
+EOT
+
+Job.create!(started: '05 Jan 2010', ended: '05 Dec 2012', title: "Web Developer", place: "University's International Multimedia Resource Center", description: <<-EOT)
+  * Determine needs of web development clients, estimate cost and time to
+  completion, delegate work amongst staff.
+  * Create web sites for University departments from scratch and using the
+  Drupal content management system.
+  * Integrate sites with University systems to access user and course data
+  using LDAP and custom integrations.
+EOT
