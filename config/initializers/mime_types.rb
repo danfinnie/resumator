0fbffae4d479a26cb6b359366209da22c1345f1a
Dan Finnie <dan@danfinnie.com>
2014-07-20 15:01:23 -0700

    Remove custom PDF mime type as it seems like Rails already has one.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,1 +1,4 @@
-Mime::Type.register "application/pdf", :pdf, ['text/pdf'], ['pdf']
+# Be sure to restart your server when you modify this file.
+
+# Add new mime types for use in respond_to blocks:
+# Mime::Type.register "text/richtext", :rtf
