e5e2b6258cd884e844612b2d415698e664270b44
Dan Finnie and Mark McDonald <pair+dfinnie+mmcdonald@pivotallabs.com>
2014-07-23 23:28:42 -0700

    WIP

---
+++ app/assets/javascripts/controllers/linked_in_controller.js
@@ -0,0 +1,52 @@
+function onLinkedInLoad() {
+  IN.Event.on(IN, "auth", function() {
+    onLinkedInLogin();
+  });
+  IN.Event.on(IN, "logout", function() {
+    onLinkedInLogout();
+  });
+}
+
+function onLinkedInLogin() {
+  // pass user info to angular
+  angular.element($("#yolo")).scope().$apply(
+    function($scope) {
+      $scope.getLinkedInData();
+    }
+  );
+}
+
+resumator.controller('LinkedInController', ['$scope', '$http', '$rootScope', '$q', function ($scope, $http, $rootScope, $q) {
+  var linkedInThings = [
+    // 'last-modified-timestamp',
+    // 'proposal-comments',
+    'associations',
+    'interests',
+    'publications',
+    'patents',
+    'languages',
+    'skills',
+    // 'certifications',
+    // 'educations',
+    // 'courses',
+    // 'volunteer',
+    // 'three-current-positions',
+    // 'three-past-positions',
+    // 'num-recommenders',
+    // 'recommendations-received',
+    // 'mfeed-rss-url',
+    // 'following',
+    // 'job-bookmarks',
+    // 'suggestions',
+    // 'date-of-birth',
+    // 'member-url-resources',
+    // 'member-url-resources:(url)',
+    // 'member-url-resources:(name)',
+    // 'related-profile-views',
+    // 'honors-awards',
+  ]
+
+  $scope.getLinkedInData = function () {
+    IN.API.Profile("me").fields(linkedInThings).result(function(){debugger; console.log(arguments[0].values[0])});
+  };
+}]);
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  get :linkedin, to: 'pages#linkedin'
+
   resources :jobs
   resources :educations
 
