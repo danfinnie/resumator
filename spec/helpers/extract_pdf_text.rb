6d471a56d0ded528a5fbfd61c63878c839c4fe23
Dan Finnie <dan@danfinnie.com>
2014-07-30 21:59:17 -0700

    Show error messages when there is a problem downloading the PDF

--- spec/helpers/extract_pdf_text.rb
+++ spec/helpers/extract_pdf_text.rb
@@ -1,8 +1,19 @@
 require 'open3'
+require 'net/http'
 
 module ExtractPdfText
   def extract_pdf_text(pdf_url)
-    pdf_text, stderr, status = Open3.capture3("curl '#{pdf_url}' | tee junk/test_output.pdf | pdftotext - -")
+    response = Net::HTTP.get_response(URI(pdf_url))
+
+    pdf_data = response.read_body
+    encoding = response.type_params["charset"]
+    pdf_data.force_encoding(encoding)
+
+    if response.code.to_i >= 400
+      raise "Failure when downloading PDF at #{pdf_url}:\nHTTP #{response.code} #{response.message}\n#{pdf_data}"
+    end
+
+    pdf_text, stderr, status = Open3.capture3("pdftotext - -", stdin_data: pdf_data)
 
     unless status.exitstatus.try(:zero?)
       raise "Curl or pdftotext failed with code #{status.exitstatus}:\n#{stderr}"
